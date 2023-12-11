c052cf1ae404a9f90169fa53629d71a31c20cc65
tlt22 <taolt1999@outlook.com>
2023-12-11 21:50:52 +0800

    add get_epsilon_controllable_set

--- controllabilitytest.py
+++ controllabilitytest.py
@@ -4,7 +4,7 @@ import numpy as np
 import gym
 
 from buffer import Buffer
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 @dataclass
 class NeighbourSet:
@@ -19,10 +19,8 @@ class  ControllabilityTest:
         self.num_sample = num_sample
         self.epsilon = epsilon
         self.lipschitz_confidence = lipschitz_confidence
-        self.onestep_matrix = np.zeros((2 * num_sample, 2 * num_sample))
-        self.reachable_matrix = None
+        self.epsilon_controllable_list: List[NeighbourSet] = []
         
-
     def sample(self, num_sample: Optional[int] = None):
         state = self.env.reset()
         num_sample = self.num_sample if num_sample is None else num_sample
@@ -32,7 +30,7 @@ class  ControllabilityTest:
             self.buffer.add((state, action, reward, next_state, done))
             state = self.env.reset() if done else next_state
 
-    def backward_expansion(self, neighbor: NeighbourSet) -> List(NeighbourSet):
+    def backward_expand(self, neighbor: NeighbourSet) -> List(NeighbourSet):
         '''
         :param neighbor (NeighbourSet): the neighbor set to be expanded
         :return (List[NeighbourSet]): the expanded neighbor set
@@ -60,19 +58,53 @@ class  ControllabilityTest:
             ) 
             for transitions in self.buffer.buffer[states_in_buffer_index]
         ]
-        
+    
+    def get_epsilon_controllable_set(self, state: np.ndarray, epsilon: float):
+        '''
+        :param state (np.ndarray): the state to be tested
+        :param epsilon (float): the epsilon value
+        '''
+        self.epsilon_controllable_list.append(NeighbourSet(state, epsilon))
+        # until all the neighbor sets are visited
+        while not all([neighbor.visited for neighbor in self.epsilon_controllable_list]):
+            for neighbor in self.epsilon_controllable_list and not neighbor.visited:
+                expand_neighbor_list = self.backward_expand(neighbor)
+                for expand_neighbor in expand_neighbor_list:                    
+                    is_repeat, idx = self.check_expand_neighbor_state_in_epsilon_controllable_list(expand_neighbor)
+                    if is_repeat:
+                        if expand_neighbor.radius > self.epsilon_controllable_list[idx].radius:
+                            self.epsilon_controllable_list[idx].radius = expand_neighbor.radius
+                    else:
+                        self.epsilon_controllable_list.append(expand_neighbor)
+
     @staticmethod
     def distance(state1: np.ndarray, state2: np.ndarray) -> float:
-        return np.linalg.norm(state1 - state2)
+        return np.linalg.norm(state1 - state2, ord=2)
     
-    def lipschitz_fx(self, state1: np.ndarray, state2: np.ndarray) -> float:
+    def check_expand_neighbor_state_in_epsilon_controllable_list(self, expand_neighbor: NeighbourSet) -> Tuple[bool, int]:
+        idx_list = []
+        for idx, neighbor in enumerate(self.epsilon_controllable_list):
+            if self.distance(expand_neighbor.centered_state, neighbor.centered_state) < 1e-6:
+                idx_list.append(idx)
+        if len(idx_list) == 0:
+            return False, -1
+        elif len(idx_list) == 1:
+            return True, idx_list[0]
+        else:
+            raise ValueError("The expand_neighbor is in the epsilon_controllable_list more than once!")
+
+    def lipschitz_fx(self, state: np.ndarray) -> float:
+        states_in_buffer_index = [
+            idx for idx, transition in enumerate(self.buffer.buffer) 
+            if self.distance(transition[0], state) <= self.lipschitz_confidence
+        ]
         # TODO: implement the lipschitz constant of the dynamics function
         return 1/2
 
     def clear(self):
-        self.onestep_matrix = np.zeros((self.num_sample, self.num_sample))
-        self.reachable_matrix = None
+        self.epsilon_controllable_list = []
         self.buffer.clear()
+
 
     def seed(self, seed=None):
         self.env.seed(seed)
